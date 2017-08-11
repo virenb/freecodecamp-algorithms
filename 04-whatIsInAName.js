e40eca016410015ae47c3be068553df9d270f415
Viren Bhagat <virenb@gmail.com>
2017-08-11 18:58:55 -0400

    what is in a name exercise with filter() and every()

---
+++ 04-whatIsInAName.js
@@ -0,0 +1,44 @@
+/* Wherefore art thou - Intermediate Algorithm Scripting - freeCodeCamp
+
+Instructions
+----
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that 
+have matching property and value pairs (second argument). Each property and value pair of the source object has 
+to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, 
+{ first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the
+third object from the array (the first argument), because it contains the property and its value, that was 
+passed on as the second argument.
+
+Here are some helpful links:
+
+Global Object
+Object.prototype.hasOwnProperty()
+Object.keys()
+*/
+*
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  var sourceKeys = Object.keys(source);
+  arr = collection.filter(function(obj) {
+		return sourceKeys.every(function(key) {
+			return obj.hasOwnProperty(key) && obj[key] === source[key]
+		});
+  });
+  
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+/* Test Cases
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) should return [{ first: "Tybalt", last: "Capulet" }].
+whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }) should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }].
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 }) should return [{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }].
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 }) should return [{ "a": 1, "b": 2, "c": 2 }].
+*/
