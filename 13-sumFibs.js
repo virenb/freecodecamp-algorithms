1ddea27f63f6493bad4fed9579f9bebd43823b59
Viren Bhagat <virenb@gmail.com>
2017-10-04 20:31:37 -0400

    Odd fibonacci exercise

---
+++ 13-sumFibs.js
@@ -0,0 +1,29 @@
+/* Sum All Odd Fibonacci Numbers - Intermediate Algorithm Scripting - freeCodeCamp
+
+Instructions
+-------
+Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than 10 are 1, 1, 3, and 5.
+
+Here are some helpful links:
+Remainder
+
+*/
+
+function sumFibs(num) {
+  return num;
+}
+
+sumFibs(4);
+
+/* Test Cases
+
+sumFibs(1) should return a number.
+sumFibs(1000) should return 1785.
+sumFibs(4000000) should return 4613732.
+sumFibs(4) should return 5.
+sumFibs(75024) should return 60696.
+sumFibs(75025) should return 135721.
+
+*/
