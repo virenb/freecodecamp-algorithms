e40d64ef2287786767beff0b6b80c26339f1d41a
Viren Bhagat <virenb@gmail.com>
2017-10-07 22:42:57 -0400

    Truth check exercise

---
+++ 20-truthCheck.js
@@ -0,0 +1,33 @@
+/* Everything Be True - Intermediate Algorithm Scripting - freeCodeCamp
+
+Instructions
+-----
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+Remember, you can access object properties through either dot notation or [] notation.
+
+*/
+
+function truthCheck(collection, pre) {
+  return collection.every(function(objs) { //use every method, cycle through objects and see if property in the second argument is true or false
+    return objs[pre];
+  });
+}
+
+truthCheck([{ "user": "Tinky-Winky", "sex": "male" }, { "user": "Dipsy", "sex": "male" }, { "user": "Laa-Laa", "sex": "female" }, { "user": "Po", "sex": "female" }], "sex");
+
+//The every() method tests whether all elements in the array pass the test implemented by the provided function.
+
+
+/* Test Cases
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return true.
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return false.
+truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age") should return false.
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat") should return false
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat") should return true
+truthCheck([{"single": "yes"}], "single") should return true
+truthCheck([{"single": ""}, {"single": "double"}], "single") should return false
+truthCheck([{"single": "double"}, {"single": undefined}], "single") should return false
+truthCheck([{"single": "double"}, {"single": NaN}], "single") should return false
+
+*/
