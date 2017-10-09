d666bd35da4dd810051456d4d5f9083aa45df0c1
Viren Bhagat <virenb@gmail.com>
2017-10-09 10:44:10 -0400

    RegEx exercise

--- 22-telephoneCheck.js
+++ 22-telephoneCheck.js
@@ -18,9 +18,10 @@ RegExp
 */
 
 function telephoneCheck(str) {
-  // Good luck!
-  return true;
+  return /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(str);
+
 }
+
 
 telephoneCheck("555-555-5555");
 
