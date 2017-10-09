c64c2b964e1e63946fbfb84af55e4cd90a891866
Viren Bhagat <virenb@gmail.com>
2017-10-09 18:39:28 -0400

    Re-added problems with correct file numbers

---
+++ 03-sym.js
@@ -0,0 +1,55 @@
+/* Symmetric Difference - Advanced Algorithm Scripting - freeCodeCamp
+
+Instructions
+-----
+Create a function that takes two or more arrays and returns an array of the symmetric difference (△ or ⊕) of the provided arrays.
+Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the mathematical term "symmetric difference" of two sets is the set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every additional symmetric difference you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
+
+Here are some helpful links:
+
+Array.prototype.reduce()
+Symmetric Difference
+
+*/
+
+function sym(args) {
+  var argsArray = Array.prototype.slice.call(arguments); // take args and put into arrays
+
+  function symDiff(arr1, arr2) {
+    var results = []; // make empty array for symmetric differences of the arrays
+
+    arr1.forEach(function(i) {
+      if (arr2.indexOf(i) < 0 && results.indexOf(i) < 0) { // cycle through array and add into results array if the items are only in arr1
+        results.push(i);
+      }
+    });
+
+    arr2.forEach(function (i) {
+      if (arr1.indexOf(i) < 0 && results.indexOf(i) < 0) { // cycle through array and add into results array if the items are only in arr2
+        results.push(i);
+      }
+    });
+
+    return results;
+  }
+
+  return argsArray.reduce(symDiff); // call symDiff function on the args
+
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]);
+
+/* Test Cases
+
+sym([1, 2, 3], [5, 2, 1, 4]) should return [3, 4, 5].
+sym([1, 2, 3], [5, 2, 1, 4]) should contain only three elements.
+sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should return [1, 4, 5]
+sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should contain only three elements.
+sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should return [1, 4, 5].
+sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should contain only three elements.
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should return [2, 3, 4, 6, 7].
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should contain only five elements.
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should return [1, 2, 4, 5, 6, 7, 8, 9].
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should contain only eight elements.
+
+*/
---
+++ 04-checkCashRegister.js
@@ -0,0 +1,50 @@
+/* Exact Change - Advanced Algorithm Scripting - freeCodeCamp
+
+Instructions
+-----------
+Design a cash register drawer function checkCashRegister() that accepts purchase price as
+the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid)
+as the third argument.
+cid is a 2D array listing available currency.
+Return the string "Insufficient Funds" if cash-in-drawer is less than the change due.
+Return the string "Closed" if cash-in-drawer is equal to the change due.
+Otherwise, return change in coin and bills, sorted in highest to lowest order.
+
+Here are some helpful links:
+Global Object - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+
+*/
+
+
+function checkCashRegister(price, cash, cid) {
+  var change;
+  // Here is your change, ma'am.
+  return change;
+}
+
+// Example cash-in-drawer array:
+// [["PENNY", 1.01],
+// ["NICKEL", 2.05],
+// ["DIME", 3.10],
+// ["QUARTER", 4.25],
+// ["ONE", 90.00],
+// ["FIVE", 55.00],
+// ["TEN", 20.00],
+// ["TWENTY", 60.00],
+// ["ONE HUNDRED", 100.00]]
+
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+
+/* Test Cases
+
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) should return an array.
+checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return a string.
+checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return a string.
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) should return [["QUARTER", 0.50]].
+checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) should return [["TWENTY", 60.00], ["TEN", 20.00], ["FIVE", 15.00], ["ONE", 1.00], ["QUARTER", 0.50], ["DIME", 0.20], ["PENNY", 0.04]].
+checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return "Insufficient Funds".
+checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1.00], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return "Insufficient Funds".
+checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return "Closed".
+
+
+*/
